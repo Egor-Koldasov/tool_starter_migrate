e09d264c4375608bcecdacce28b78f89697558f2
Egor Koldasov <koldasov3@gmail.com>
2021-11-03 16:10:06 +0300

    add test version

--- knexfile.ts
+++ knexfile.ts
@@ -1,6 +1,16 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+export const importEnv = (dirname: string, relPath: string) => dotenv.config({'path': path.resolve(dirname, relPath)});
+
+if (process.env.NODE_ENVIRONMENT === 'test') {
+  importEnv(__dirname, './test.env');
+}
+importEnv(__dirname, './dev.env');
+
 module.exports = {
   client: 'pg',
-  connection: process.env.DATABASE_URL || 'postgres://admin:devpassword@localhost:5432/dev_overview?sslmode=disable',
+  connection: process.env.DATABASE_URL,
   migrations: {
     tableName: 'migrations',
     directory: 'src/migrations',

ba59786d29275f5a1be2daed6de5bcc449d8dd5a
Egor Koldasov <koldasov3@gmail.com>
2021-11-04 12:44:36 +0300

    move in database creation

--- knexfile.ts
+++ knexfile.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import knex from 'knex';
 import path from 'path';
 
 export const importEnv = (dirname: string, relPath: string) => dotenv.config({'path': path.resolve(dirname, relPath)});
@@ -8,12 +9,42 @@ if (process.env.NODE_ENVIRONMENT === 'test') {
 }
 importEnv(__dirname, './dev.env');
 
-module.exports = {
-  client: 'pg',
-  connection: process.env.DATABASE_URL,
-  migrations: {
-    tableName: 'migrations',
-    directory: 'src/migrations',
-    extension: 'ts',
-  }
+const dbConfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
+
+// create database if not exists
+export const initDatabase = async () => {
+  const db = knex({
+    client: 'pg',
+    connection: {
+      ...dbConfig,
+      database: 'postgres',
+    },
+  });
+  const res = await db.raw(
+    `SELECT datname FROM pg_catalog.pg_database WHERE datname='${dbConfig.database}'`
+  );
+  if (res.rows.length === 0) {
+    await db.raw(`CREATE DATABASE "${dbConfig.database}"`);
+  }
+  await db.destroy();
+}
+
+const setupKnex = async () => {
+  await initDatabase();
+  return {
+    client: 'pg',
+    connection: dbConfig,
+    migrations: {
+      tableName: 'migrations',
+      directory: 'src/migrations',
+      extension: 'ts',
+    }
+  };
+}
+
+export default setupKnex
